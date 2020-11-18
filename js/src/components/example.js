d0242a6cb3783cc3e3a7bcfa9fdf55f30caa75c2
Ryan Kienstra <kienstraryan@gmail.com>
2020-11-18 14:50:44 -0600

    Correct a typo in the example block

--- js/src/components/example.js
+++ js/src/components/example.js
@@ -22,7 +22,7 @@ import { __ } from '@wordpress/i18n';
 const Example = ( { attributes } ) => (
 	<div>
 		<p>{ __( 'This is an alternate preview component', 'headless-blocks' ) }</p>
-		<p>{ __( 'Here is the valus of the new-field attribute, if it exists:', 'headless-blocks' ) }</p>
+		<p>{ __( 'Here is the value of the new-field attribute, if it exists:', 'headless-blocks' ) }</p>
 		<p>{ attributes[ 'new-field' ] }</p>
 	</div>
 );
