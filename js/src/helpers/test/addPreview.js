3ad5b5d42a2e0dad5b9f3b652e6cfc9f997a4fbf
Ryan Kienstra <kienstraryan@gmail.com>
2020-11-20 11:23:35 -0600

    Update the Jest test

--- js/src/helpers/test/addPreview.js
+++ js/src/helpers/test/addPreview.js
@@ -4,7 +4,11 @@
 import { addPreview } from '../';
 
 describe( 'addPreview', () => {
-	it( 'should not add a preview for the block because it has the wrong namespace', () => {
-		expect(	addPreview( null, 'example', {} ).length ).toStrictEqual( 1 );
+	it( 'should add a preview of a block that is available', () => {
+		expect(	addPreview( null, 'email-opt-in', {} ).length ).toStrictEqual( 1 );
+	} );
+
+	it( 'should not add a preview for the block because it is not available', () => {
+		expect(	addPreview( null, 'block-name-not-available', {} ) ).toStrictEqual( null );
 	} );
 } );
