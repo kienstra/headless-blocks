4d847b3fe0abb47cae4fbdac92ed601172f25dfa
Ryan Kienstra <kienstraryan@gmail.com>
2021-02-26 19:22:15 -0600

    Update tsconfig.json, set .nvmrc to 15
    It looks like npm is needed
    for workspaces in package.json

--- js/src/helpers/addPreview.ts
+++ js/src/helpers/addPreview.ts
@@ -6,14 +6,16 @@ import * as React from 'react';
 /**
  * Internal dependencies
  */
-import { blockComponents } from 'headless-block-components';
+import blockComponents from '@getting-started/blocks/dist';
 
 /**
  * Adds a preview component to the filter.
  */
-const addPreview = ( initialPreview: React.Component[] | null, blockName: string ): React.Component[] | null | any => {
+const addPreview = ( initialPreview: React.Component[] | null, blockName: String ): React.Component[] | null | any => {
 	const fullBlockName = `genesis-custom-blocks/${ blockName }`;
-	return blockComponents[ fullBlockName ] ? blockComponents[ fullBlockName ] : initialPreview; /* eslint-disable-line import/namespace */
+	// @ts-ignore
+	const block = blockComponents[ fullBlockName ] ? blockComponents[ fullBlockName ] : initialPreview;
+	return block /* eslint-disable-line import/namespace */
 };
 
 export default addPreview;
